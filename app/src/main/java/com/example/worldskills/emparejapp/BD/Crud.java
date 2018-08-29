d92c097c7cec310f4ade5f88410d94d55fb0ad20
Daniel <dani@gmail.com>
2018-08-29 13:56:56 -0500

    s

--- app/src/main/java/com/example/worldskills/emparejapp/BD/Crud.java
+++ app/src/main/java/com/example/worldskills/emparejapp/BD/Crud.java
@@ -1,4 +1,42 @@
 package com.example.worldskills.emparejapp.BD;
 
-public class Crud {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Crud extends SQLiteOpenHelper {
+    public Crud(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tb_puntaje(id integer primary key autoincrement,nombre text,puntaje integer,tiempo text)");
+        db.execSQL("create table tb_config(id integer primary key autoincrement, minutos text,segundos text,siTiempo integer)");
+        ContentValues registro1=new ContentValues();
+        for (int i=0;i<12;i++){
+            ContentValues registro=new ContentValues();
+            registro.put("nombre","nadie");
+            registro.put("puntaje","0");
+            registro.put("tiempo","00:00");
+            db.insert("tb_puntaje",null,registro);
+        }
+        registro1.put("minutos","0");
+        registro1.put("segundos","0");
+        registro1.put("siTiempo","0");
+        db.insert("tb_config",null,registro1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists tb_puntaje");
+        db.execSQL("drop table if exists tb_config");
+        db.execSQL("create table tb_puntaje(id integer primary key autoincrement,nombre text,puntaje integer,tiempo text)");
+        db.execSQL("create table tb_config(id integer primary key autoincrement, minutos text,segundos text,siTiempo integer)");
+    }
+    public void iniciarDB(Context context){
+        Crud crud=new Crud(context,"emparejados",null,1);
+        SQLiteDatabase db=crud.getWritableDatabase();
+    }
 }
--- app/src/main/java/com/example/worldskills/emparejapp/SplashScreen.java
+++ app/src/main/java/com/example/worldskills/emparejapp/SplashScreen.java
@@ -3,11 +3,15 @@ package com.example.worldskills.emparejapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.worldskills.emparejapp.BD.Crud;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Crud crud=new Crud(this,"emparejados",null,1);
+        crud.iniciarDB(this);
     }
 }
