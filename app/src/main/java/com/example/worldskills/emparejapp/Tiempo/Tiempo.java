9deffd57e8c9e0a4b1e084e99ccbdd5f5976beca
Alirio <alirio@gmail.com>
2018-08-29 15:21:04 -0500

    f

--- app/src/main/java/com/example/worldskills/emparejapp/Tiempo/Tiempo.java
+++ app/src/main/java/com/example/worldskills/emparejapp/Tiempo/Tiempo.java
@@ -1,4 +1,16 @@
 package com.example.worldskills.emparejapp.Tiempo;
 
+import android.os.SystemClock;
+import android.widget.Chronometer;
+
 public class Tiempo {
+
+    long detenerse;
+    Boolean running = false;
+
+    public void iniciarchronometro(Chronometer chronometro){
+        if (running){
+            chronometro.setBase(SystemClock.elapsedRealtime() - detenerse);
+        }
+    }
 }
