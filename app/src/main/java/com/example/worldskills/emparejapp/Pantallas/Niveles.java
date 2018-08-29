b1c56772d9aa3c9b160d185b4ac5d18848598481
Alirio <alirio@gmail.com>
2018-08-29 15:53:35 -0500

    r

--- app/src/main/java/com/example/worldskills/emparejapp/Pantallas/Niveles.java
+++ app/src/main/java/com/example/worldskills/emparejapp/Pantallas/Niveles.java
@@ -1,15 +1,19 @@
 package com.example.worldskills.emparejapp.Pantallas;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.worldskills.emparejapp.R;
+import com.example.worldskills.emparejapp.Tiempo.Tiempo;
 
 public class Niveles extends AppCompatActivity {
     Button facil,medio,dificil;
+    TextView temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,15 @@ public class Niveles extends AppCompatActivity {
         facil=findViewById(R.id.facilId);
         medio=findViewById(R.id.medioId);
         dificil=findViewById(R.id.dificilId);
+        temp = findViewById(R.id.temp);
+        final Tiempo iniciar = new Tiempo();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iniciar.temporizador(temp, 1,30);
+            }
+        },1000);
+
         facil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
